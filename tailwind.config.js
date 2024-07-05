2b4515da2536ec7468b13e0823d977f592d120b9
Connor McIntire <connorjmcintire@gmail.com>
2024-07-04 23:55:28 -0400

    Update
    Updated the website to look better

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+    './js/*.js',
+    './CSS/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
